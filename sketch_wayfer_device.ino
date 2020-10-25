0794db0714baa7dae31644090f9b5568b29fe6b3
HwangYS1 <72289516+HwangYS1@users.noreply.github.com>
2020-10-25 18:14:07 +0900

    2020/10/25 middle build

---
+++ sketch_wayfer_device.ino
@@ -0,0 +1,81 @@
+#include <Wire.h>
+#include <VL53L1X.h>
+#include <SoftwareSerial.h>
+#define BT_RXD 8
+#define BT_TXD 7
+
+SoftwareSerial BtSerial(BT_RXD, BT_TXD);
+
+VL53L1X sensor;
+
+unsigned long time_previous, time_current;
+
+void setup()
+{
+  Serial.begin(115200);
+  Wire.begin();
+  Wire.setClock(400000);
+  sensor.setTimeout(500);
+  BtSerial.begin(115200);
+  time_previous = millis();
+ 
+  sensor.setDistanceMode(VL53L1X::Long);
+  sensor.setMeasurementTimingBudget(50000);
+  sensor.startContinuous(50);
+}
+
+void loop()
+{
+  time_current = millis();
+  double cm = sensor.read();
+ 
+  Serial.print(sensor.read());
+
+  if(time_current - time_previous >= 1000){
+     BtSerial.print(cm);
+     BtSerial.println();
+     }
+}#include <Wire.h>
+#include <VL53L1X.h>
+#include <SoftwareSerial.h>
+#define BT_RXD 8
+#define BT_TXD 7
+
+SoftwareSerial BtSerial(BT_RXD, BT_TXD);
+
+VL53L1X sensor;
+
+unsigned long time_previous, time_current;
+
+void setup()
+{
+  Serial.begin(115200);
+  Wire.begin();
+  Wire.setClock(400000);
+  sensor.setTimeout(500);
+  BtSerial.begin(115200);
+  time_previous = millis();
+ 
+  if (!sensor.init())
+  {
+    Serial.println("Failed to detect and initialize sensor!");
+    while (1);
+  }
+ 
+  sensor.setDistanceMode(VL53L1X::Long);
+  sensor.setMeasurementTimingBudget(50000);
+  sensor.startContinuous(50);
+}
+
+void loop()
+{
+  time_current = millis();
+  double cm = sensor.read();
+ 
+  Serial.print(sensor.read());
+
+  if(time_current - time_previous >= 1000){
+     BtSerial.print(cm);
+     BtSerial.println();
+     }
+}
